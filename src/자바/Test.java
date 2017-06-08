b12b3cf002c9970adf4cb44af8b031939b410e13
nice7677 <nice7677@naver.com>
2017-06-08 14:53:20 +0900

    asd

--- src/자바/Test.java
+++ src/자바/Test.java
@@ -4,10 +4,6 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("테스트ㄴㅇㄹㄴㅇㄹㄴㅇㄹㄴㅇ랑ㄴ리ㅏㅣㅇasdasdasdasdㅁㄴㅇㅁㄴ입니다.");
-		System.out.println("방금 수정함");
 		System.out.println("asdasd");
-		System.out.println("asdasdasd");
-		System.out.println("한 번더");
 	}
 }
