bb45f001dc536024854f1e694b2a95a9adadff94
nice7677 <nice7677@naver.com>
2017-06-08 14:06:13 +0900

    ㄴㅇㄹㄴㅇㄹ
    ㄴㅇㄹ

--- src/자바/Test.java
+++ src/자바/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("테스트ㄴㅇㄹㄴㅇㄹㄴㅇㄹㄴㅇ랑ㄴ리ㅏㅣㅇ입니다.");
+		System.out.println("테스트ㄴㅇㄹㄴㅇㄹㄴㅇㄹㄴㅇ랑ㄴ리ㅏㅣㅇasdasdasdasdㅁㄴㅇㅁㄴ입니다.");
 		System.out.println("불라불라");
 		System.out.println("불라불라");
 		System.out.println("불라불라");
