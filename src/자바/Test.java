a8e47adf119f66e73837ff7e31f10ebaf5c78fa3
nice7677 <nice7677@naver.com>
2017-06-08 14:39:21 +0900

    sd

--- src/자바/Test.java
+++ src/자바/Test.java
@@ -5,7 +5,6 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("테스트ㄴㅇㄹㄴㅇㄹㄴㅇㄹㄴㅇ랑ㄴ리ㅏㅣㅇasdasdasdasdㅁㄴㅇㅁㄴ입니다.");
-
 		System.out.println("ASdasdasd");
 
 	}
