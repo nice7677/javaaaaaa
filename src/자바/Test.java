e64230fa5c6f90b8ebbb2e802c93fd661457a627
icarus8050 <icarus8050@naver.com>
2017-06-08 14:46:44 +0900

    1112

--- src/자바/Test.java
+++ src/자바/Test.java
@@ -6,7 +6,6 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("테스트ㄴㅇㄹㄴㅇㄹㄴㅇㄹㄴㅇ랑ㄴ리ㅏㅣㅇasdasdasdasdㅁㄴㅇㅁㄴ입니다.");
 		System.out.println("방금 수정함");
-		System.out.println("asdasd");
-		System.out.println("한 번더");
+		System.out.println("윤철");
 	}
 }
