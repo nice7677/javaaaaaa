ef7823f04c2037c502a8d1273202ee7bd8da2c42
icarus8050 <icarus8050@naver.com>
2017-06-08 14:49:59 +0900

    123
    123

--- src/자바/Test.java
+++ src/자바/Test.java
@@ -5,13 +5,5 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("테스트ㄴㅇㄹㄴㅇㄹㄴㅇㄹㄴㅇ랑ㄴ리ㅏㅣㅇasdasdasdasdㅁㄴㅇㅁㄴ입니다.");
-		System.out.println("방금 수정함");
-<<<<<<< HEAD
-		System.out.println("윤철");
-=======
-		System.out.println("asdasd");
-		System.out.println("asdasdasd");
-		System.out.println("한 번더");
->>>>>>> origin/master
 	}
 }
