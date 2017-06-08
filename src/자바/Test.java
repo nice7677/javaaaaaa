8ab9fc66eb2955337417157737fbe1a0d308be9c
icarus8050 <icarus8050@naver.com>
2017-06-08 14:23:32 +0900

    t삭제
    삭제

--- src/자바/Test.java
+++ src/자바/Test.java
@@ -5,9 +5,6 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("테스트ㄴㅇㄹㄴㅇㄹㄴㅇㄹㄴㅇ랑ㄴ리ㅏㅣㅇasdasdasdasdㅁㄴㅇㅁㄴ입니다.");
-		System.out.println("불라불라");
-		System.out.println("불라불라");
-		System.out.println("불라불라");
 
 	}
 
