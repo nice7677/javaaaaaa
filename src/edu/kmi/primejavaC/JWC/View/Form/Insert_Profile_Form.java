1d1d335c341cb34dea669f15c11413d1e8485e8b
icarus8050 <icarus8050@naver.com>
2017-06-20 00:00:31 +0900

    ㅍㅇㄴ

--- src/edu/kmi/primejavaC/JWC/View/Form/Insert_Profile_Form.java
+++ src/edu/kmi/primejavaC/JWC/View/Form/Insert_Profile_Form.java
@@ -31,6 +31,8 @@ import java.awt.event.ItemListener;
 import java.awt.event.ActionEvent;
 
 public class Insert_Profile_Form extends Parent_Form {
+	public final static int EDIT_MODE = 1;
+	
 	private JTextField txtName;
 	private JTextField txtAge;
 	private JTextField txtPhone;
@@ -43,6 +45,7 @@ public class Insert_Profile_Form extends Parent_Form {
 
 	/**
 	 * Create the frame.
+	 * @wbp.parser.constructor
 	 */
 	public Insert_Profile_Form(FrontController control, Member account) {
 		super(550, 800, control);
@@ -205,6 +208,7 @@ public class Insert_Profile_Form extends Parent_Form {
 				// TODO Auto-generated method stub
 				JComboBox ch = (JComboBox) e.getSource();
 				account.setRegion(ch.getSelectedItem().toString());
+				System.out.println(ch.getSelectedItem().toString());
 			}
 		});
 		
@@ -215,7 +219,7 @@ public class Insert_Profile_Form extends Parent_Form {
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
 				JComboBox ch = (JComboBox) e.getSource();
-				account.setRegion(ch.getSelectedItem().toString());
+				account.setBloodT(ch.getSelectedItem().toString());
 			}
 		});
 			//상대방 혈액형 선택 콤보박스 이벤트
@@ -225,7 +229,7 @@ public class Insert_Profile_Form extends Parent_Form {
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
 				JComboBox ch = (JComboBox) e.getSource();
-				account.setRegion(ch.getSelectedItem().toString());
+				account.setMyTypeB(ch.getSelectedItem().toString());
 			}
 		});
 		
@@ -269,6 +273,210 @@ public class Insert_Profile_Form extends Parent_Form {
 				account.setGender("여자");
 			}
 		}
+		
+	}
+	
+	public Insert_Profile_Form(FrontController control, Member account, int mode) {
+		super(550, 800, control);
+		setTitle("InsertProfilePage");
+		
+		this.account = account;
+		
+		//------------컴포넌트 구성------------------------
+		getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		JPanel insert_profile_panel = new JPanel();
+		getContentPane().add(insert_profile_panel, BorderLayout.CENTER);
+		insert_profile_panel.setLayout(null);
+		
+		JLabel lblPleaseFillOut = new JLabel("Please fill out the profile below!");
+		lblPleaseFillOut.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPleaseFillOut.setFont(new Font("Yu Gothic", Font.BOLD, 20));
+		lblPleaseFillOut.setBounds(103, 54, 332, 31);
+		insert_profile_panel.add(lblPleaseFillOut);
+		
+		JLabel lblNewLabel = new JLabel("Name");
+		lblNewLabel.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblNewLabel.setBounds(105, 112, 72, 31);
+		insert_profile_panel.add(lblNewLabel);
+		
+		JLabel lblAge = new JLabel("Age");
+		lblAge.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblAge.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+		lblAge.setBounds(105, 168, 72, 31);
+		insert_profile_panel.add(lblAge);
+		
+		JLabel lblGender = new JLabel("Gender");
+		lblGender.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblGender.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+		lblGender.setBounds(105, 224, 72, 31);
+		insert_profile_panel.add(lblGender);
+		
+		JLabel lblRegion = new JLabel("Region");
+		lblRegion.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblRegion.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+		lblRegion.setBounds(105, 282, 72, 31);
+		insert_profile_panel.add(lblRegion);
+		
+		JLabel lblPhone = new JLabel("Phone");
+		lblPhone.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblPhone.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+		lblPhone.setBounds(105, 347, 72, 31);
+		insert_profile_panel.add(lblPhone);
+		
+		JLabel lblIntroduce = new JLabel("Introduce");
+		lblIntroduce.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblIntroduce.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+		lblIntroduce.setBounds(105, 604, 72, 31);
+		insert_profile_panel.add(lblIntroduce);
+		
+		JLabel lblBloodtype = new JLabel("BloodType");
+		lblBloodtype.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblBloodtype.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+		lblBloodtype.setBounds(90, 408, 87, 31);
+		insert_profile_panel.add(lblBloodtype);
+		
+		JLabel lblCharacter = new JLabel("Character");
+		lblCharacter.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblCharacter.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+		lblCharacter.setBounds(90, 474, 87, 31);
+		insert_profile_panel.add(lblCharacter);
+		
+		JLabel lblIdealbloodtype = new JLabel("Ideal_BloodType");
+		lblIdealbloodtype.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblIdealbloodtype.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+		lblIdealbloodtype.setBounds(56, 542, 121, 31);
+		insert_profile_panel.add(lblIdealbloodtype);
+		
+		txtName = new JTextField();
+		txtName.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+		txtName.setBounds(285, 112, 116, 31);
+		insert_profile_panel.add(txtName);
+		txtName.setColumns(10);
+		
+		txtAge = new JTextField();
+		txtAge.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+		txtAge.setColumns(10);
+		txtAge.setBounds(285, 168, 116, 31);
+		insert_profile_panel.add(txtAge);
+		
+		txtPhone = new JTextField();
+		txtPhone.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+		txtPhone.setColumns(20);
+		txtPhone.setBounds(268, 350, 145, 31);
+		insert_profile_panel.add(txtPhone);
+		
+		ButtonGroup group = new ButtonGroup();
+		rdoMale = new JRadioButton("Male");
+		rdoMale.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+		rdoMale.setBounds(268, 226, 72, 27);
+		insert_profile_panel.add(rdoMale);
+		
+		rdoFemale = new JRadioButton("Female");
+		rdoFemale.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+		rdoFemale.setBounds(346, 226, 89, 27);
+		insert_profile_panel.add(rdoFemale);
+		
+		group.add(rdoMale);
+		group.add(rdoFemale);
+		
+		JComboBox cboRegion = new JComboBox();
+		cboRegion.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+		cboRegion.setMaximumRowCount(7);
+		cboRegion.setModel(new DefaultComboBoxModel(new String[] {"서울", "경기도", "강원도", "충청도", "경상도", "전라도", "제주도"}));
+		cboRegion.setSelectedIndex(0);
+		cboRegion.setBounds(268, 284, 145, 31);
+		insert_profile_panel.add(cboRegion);
+		account.setRegion(cboRegion.getSelectedItem().toString());
+		
+		JComboBox cboBloodType = new JComboBox();
+		cboBloodType.setModel(new DefaultComboBoxModel(new String[] {"A", "B", "O", "AB"}));
+		cboBloodType.setSelectedIndex(0);
+		cboBloodType.setMaximumRowCount(4);
+		cboBloodType.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+		cboBloodType.setBounds(268, 408, 145, 31);
+		insert_profile_panel.add(cboBloodType);
+		account.setBloodT(cboBloodType.getSelectedItem().toString());
+		
+		JComboBox cboIdeal_BloodType = new JComboBox();
+		cboIdeal_BloodType.setModel(new DefaultComboBoxModel(new String[] {"A", "B", "O", "AB"}));
+		cboIdeal_BloodType.setSelectedIndex(0);
+		cboIdeal_BloodType.setMaximumRowCount(4);
+		cboIdeal_BloodType.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+		cboIdeal_BloodType.setBounds(268, 547, 145, 31);
+		insert_profile_panel.add(cboIdeal_BloodType);
+		account.setMyTypeB(cboIdeal_BloodType.getSelectedItem().toString());
+		
+		txtIntroduce = new JTextArea();
+		txtIntroduce.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+		txtIntroduce.setBounds(222, 610, 248, 70);
+		insert_profile_panel.add(txtIntroduce);
+		
+		txtCharacter = new JTextField();
+		txtCharacter.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+		txtCharacter.setBounds(285, 479, 116, 31);
+		insert_profile_panel.add(txtCharacter);
+		txtCharacter.setColumns(10);
+		
+		JButton btnInsert = new JButton("Insert");
+		btnInsert.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+		btnInsert.setBounds(213, 703, 105, 27);
+		insert_profile_panel.add(btnInsert);
+		//------------------컴포넌트 구성 End-----------------------------
+		
+		txtName.setText(account.getName());
+		txtAge.setText(account.getAge().toString());
+		txtPhone.setText(account.getPhone().toString());
+		if(account.getGender().equals("남자"))
+			rdoMale.setSelected(true);
+		else
+			rdoFemale.setSelected(true);
+		cboRegion.setSelectedItem(account.getRegion());
+		cboIdeal_BloodType.setSelectedItem(account.getMyTypeB());
+		cboBloodType.setSelectedItem(account.getBloodT());
+		txtIntroduce.setText(account.getIntro());
+		txtCharacter.setText(account.getMyType());
+		
+		
+		//------------------이벤트 등록=--------------------------------
+		rdoMale.addItemListener(new RadioListener());
+		rdoFemale.addItemListener(new RadioListener());
+		
+			//지역 선택 콤보박스 이벤트
+		cboRegion.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				JComboBox ch = (JComboBox) e.getSource();
+				account.setRegion(ch.getSelectedItem().toString());
+			}
+		});
+		
+			//자신의 혈액형 선택 콤보박스 이벤트
+		cboBloodType.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				JComboBox ch = (JComboBox) e.getSource();
+				account.setBloodT(ch.getSelectedItem().toString());
+			}
+		});
+			//상대방 혈액형 선택 콤보박스 이벤트
+		cboIdeal_BloodType.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				JComboBox ch = (JComboBox) e.getSource();
+				account.setMyTypeB(ch.getSelectedItem().toString());
+			}
+		});
+		
+			//입력 버튼 이벤트
+		btnInsert.addActionListener(new ProfileInsertEvent(this));
 		
 	}
 }
--- src/edu/kmi/primejavaC/JWC/View/Form/Main_Form.java
+++ src/edu/kmi/primejavaC/JWC/View/Form/Main_Form.java
@@ -4,35 +4,212 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Toolkit;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+
+import com.sun.xml.internal.ws.api.streaming.XMLStreamReaderFactory.Default;
 
 import edu.kmi.primejavaC.JWC.Controller.FrontController;
+import edu.kmi.primejavaC.JWC.Controller.Event.SearchEvent;
 import edu.kmi.primejavaC.JWC.Model.Member;
+import javafx.scene.layout.Border;
 
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
+import javax.swing.JToolBar;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.GridBagLayout;
+import javax.swing.JTable;
+import java.awt.GridLayout;
+import javax.swing.JTextField;
+import java.awt.FlowLayout;
+import javax.swing.SwingConstants;
+import javax.swing.JRadioButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JSeparator;
+import javax.swing.JDesktopPane;
 
 public class Main_Form extends Parent_Form {
 	/**
 	 * Create the frame.
 	 */
+	JPanel panData;
+	DefaultTableModel model;
+	JTable table;
+	final String colName[] = {"아이디", "이름", "나이", "성격", "지역", "소개"};
+	private JTextField txtAge_1;
+	private JTextField txtAge_2;
+	
 	public Main_Form(FrontController control, Member info) {
 		super(1024, 768, control);
 		setInfo(info);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		setTitle("Find your ideal-type!!");
 		
+		//----------------------컴포넌트 구성-----------------------
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 		
 		JMenu mnAccount = new JMenu("Account");
 		menuBar.add(mnAccount);
 		
-		JMenuItem miSignout = new JMenuItem("SignOut");
+		JMenuItem miSignout = new JMenuItem("LogOut");
 		mnAccount.add(miSignout);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("EditProfile");
+		mntmNewMenuItem.setFont(new Font("Malgun Gothic", Font.PLAIN, 15));
+		mnAccount.add(mntmNewMenuItem);
+		getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		panData = new JPanel();
+		panData.setBounds(52, 145, 886, 524);
+		panel.add(panData);
+		panData.setLayout(new BorderLayout(0, 0));
+		
+		model = new DefaultTableModel(colName, 0);
+		table = new JTable(model);
+		table.setFont(new Font("맑은 고딕", Font.PLAIN, 17));
+		JScrollPane scrollPane = new JScrollPane(table);
+		panData.add(scrollPane, BorderLayout.CENTER);
+		
+		JLabel lblGreeting = new JLabel();
+		lblGreeting.setHorizontalAlignment(SwingConstants.CENTER);
+		lblGreeting.setFont(new Font("맑은 고딕", Font.BOLD, 17));
+		lblGreeting.setBounds(709, 30, 229, 43);
+		panel.add(lblGreeting);
+		lblGreeting.setText(info.getName() + " 님 환영합니다!");
+		
+		JLabel lblNewLabel = new JLabel("Partner's Infomation");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.LEFT);
+		lblNewLabel.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+		lblNewLabel.setBounds(52, 97, 229, 36);
+		panel.add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("Message");
+		btnNewButton.setBounds(52, 58, 105, 27);
+		panel.add(btnNewButton);
+		
+		JLabel lblNewLabel_3 = new JLabel(" 나이 ");
+		lblNewLabel_3.setBounds(595, 108, 44, 22);
+		panel.add(lblNewLabel_3);
+		lblNewLabel_3.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+		
+		txtAge_1 = new JTextField();
+		txtAge_1.setBounds(665, 104, 66, 31);
+		panel.add(txtAge_1);
+		txtAge_1.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+		txtAge_1.setColumns(10);
+		
+		txtAge_2 = new JTextField();
+		txtAge_2.setBounds(777, 104, 66, 31);
+		panel.add(txtAge_2);
+		txtAge_2.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+		txtAge_2.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel(" ~ ");
+		lblNewLabel_2.setBounds(742, 108, 23, 22);
+		panel.add(lblNewLabel_2);
+		lblNewLabel_2.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+		
+		JButton btnSearch = new JButton("검 색");
+		btnSearch.setBounds(857, 105, 81, 29);
+		panel.add(btnSearch);
+		btnSearch.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+		
+			//검색기능
+		btnSearch.addActionListener(new SearchEvent(this));
+		//---------------------------컴포넌트 구성 End ----------------------------------
+		
+		//---------------------------이벤트 등록---------------------------------------
+		mntmNewMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				dispose();
+				Insert_Profile_Form insert_form = new Insert_Profile_Form(getControl(), info, Insert_Profile_Form.EDIT_MODE);
+			}
+		});
+		
+			//로그아웃 기능
+		miSignout.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+				Login_Form login = new Login_Form(getControl());
+			}
+		});
+		//----------------------------이벤트 등록 End ------------------------------------
+		
+		//자신에게 맞는 이성을 랜덤으로 JTable에 출력
+		try {
+			ArrayList<Member> list = partner_Information();	//자신의 이상형 데이터 가져오는 함수
+			make_Component(list);
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}
+	
+	//자신의 이상형 데이터 가져오는 함수
+	public ArrayList<Member> partner_Information() throws SQLException{
+		String gender;
+		ArrayList<Member> list;
+		if(getInfo().getGender().equals("남자"))
+			gender = "여자";
+		else
+			gender = "남자";
+		
+		getControl().open();
+		list = getControl().typelist(gender, getInfo().getMyTypeB(), getInfo().getRegion());
+		getControl().close();
+		
+		return list;
+	}
+	
+	public void make_Component(ArrayList<Member> list){
+		for(int i = 0; i < list.size(); i++){
+			if(list.size() == 0){
+				JOptionPane.showMessageDialog(null, "조건에 만족하는 상대방이 없습니다.");
+				return;
+			}
+			String[] row = {list.get(i).getId(), list.get(i).getName(), list.get(i).getAge().toString(), 
+					list.get(i).getMyType(), list.get(i).getRegion(), list.get(i).getIntro()};
+			
+			model.addRow(row);
+		}
+	}
+	
+	public int[] getAgeCheck(){
+		int age[] = new int[2];
+		try{
+			age[0] = Integer.parseInt(txtAge_1.getText());
+		}catch (NumberFormatException e) {
+			age[0] = 0;
+		}
+		
+		try{
+			age[1] = Integer.parseInt(txtAge_2.getText());
+		}catch (NumberFormatException e){
+			age[1] = 10000;
+		}
+		
+		return age;
 	}
 }
